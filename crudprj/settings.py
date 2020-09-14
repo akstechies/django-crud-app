4310fe5532cf2ca32693ef169cf74712627b88a9
unknown <ayushasri95@gmail.com>
2020-09-14 10:52:30 +0530

    make it better

--- crudprj/settings.py
+++ crudprj/settings.py
@@ -1,4 +1,5 @@
 import django_heroku
+import os
 """
 Django settings for crudprj project.
 
@@ -18,12 +19,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'enroll')
+STATIC_URL = 'enroll/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'enroll/static'),
 )
 
 
